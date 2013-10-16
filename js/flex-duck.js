8fd3048781db82e7c836b543bf4ba282cf245838
André Kallehauge <andrekallehauge@gmail.com>
2013-10-16 03:08:39 +0200

    All functions working except when reducing screen size

--- js/flex-duck.js
+++ js/flex-duck.js
@@ -4,19 +4,17 @@
  * ===========================================
  */
 
-function initFlexDuck(gridColumns, medium, large) {
-	// Make sure it's Integers and pass the variable as null if it isn't set.
+function initFlexDuck(gridColumns, medium, large, rowMaxWidth) {
+	// Make sure the parameters are integers and pass the variable as null if it isn't set.
 	gridColumns = parseInt(gridColumns) || null;
 	medium 		= parseInt(medium) 		|| null;
 	large 		= parseInt(large) 		|| null;
+	rowMaxWidth = parseInt(rowMaxWidth) || null;
 
-	// Row: set width to 100% and clear fix at the end of each row.
-	$(".row").css({
-		"width": "100%",
-		"-webkit-box-sizing": "border-box",
-		"-moz-box-sizing": "border-box",
-		"box-sizing": "border-box"
-	}).after('<div style="clear:both; height:0;"></div>');
+	// Row: set max width, if a 4. parameter is given.
+	if (rowMaxWidth != null) {
+		$(".row").css("max-width", rowMaxWidth);
+	}
 
 	// Function: to set the width of the different classes (based on the amount of columns and class name)
 	function queryWidth(columns, name) {
@@ -31,33 +29,64 @@ function initFlexDuck(gridColumns, medium, large) {
 	queryWidth(gridColumns, "medium");
 	queryWidth(gridColumns, "large");
 
-	// The "media queries"
+	// Large: what should happen if the browser is greater than "large" ?
+	function largeScreen() {
+		// add classes
+		$(".large-hide").addClass("large-hidden");
+		// remove classes
+		$(".medium-hide").removeClass('medium-hidden');
+		$(".small-hide").removeClass('small-hidden');
+	}
+
+	// Medium: what should happen if the browser is greater than "medium" ?
+	function mediumScreen() {
+		// add classes
+		$(".medium-hide").addClass('medium-hidden');
+		// remove classes
+		$(".large-hide").removeClass('large-hidden');
+		$(".small-hide").removeClass('small-hidden');
+	}
+
+	// Small: what should happen if the browser is greater than "medium" ?
+	function smallScreen() {
+		// add classes
+		$(".small-hide").addClass("small-hidden");
+		// remove classes
+		$(".large-hide").removeClass('large-hidden');
+		$(".medium-hide").removeClass('medium-hidden');
+	}
+
+	// The "media queries".
 	function displayQueryClasses() {
-		// Width of the browser window
+		// Width of the browser window.
 		windowWidth = $(window).width();
 
 		// Media query hierarchy
 		if (windowWidth >= large) {
-			// console.log("large");
-			largeClass = $("[class*=large-]");
-			$(".medium-hide").addClass('small-hidden');
-		}
-		else if (windowWidth >= medium) {
-			// console.log("medium");
-			$(".small-hide").addClass('small-hidden');
-			// Add styles for the queries
-	$(".small-hidden").css("cssText", "display: none !important");
-		}
-		else {
-			$(".small-hide").removeClass("small-hidden");
-			console.log("small");
+			/**
+			 *	Large: if the browser-window is greater than "large", then
+			 *	it will execute a function defined above with styling.
+			 */
+			largeScreen();
+		} else if (windowWidth >= medium) {
+			/**
+			 *	Medium: if the browser-window is greater than "medium", then
+			 *	it will execute a function defined above with styling.
+			 */
+			mediumScreen();
+		} else {
+			/**
+			 *	Small: if the browser-window is less than "small", then
+			 *	it will execute a function defined above with styling.
+			 */
+			smallScreen();
 		}
 	}
 
-	// Initiate the function on load
+	// Initiate the "What to display" function on load.
 	displayQueryClasses();
 
-	// Initiate the function when the windows is being resized.
+	// Initiate the "What to display" function when the window is being resized.
 	$(window).resize(function() {
 		displayQueryClasses();
 	});

cf330b714e751bea2d6ccffe773ee12d4a1e6bdb
André Kallehauge <andrekallehauge@gmail.com>
2013-10-18 01:40:48 +0200

    flex-duck.js: Changed variable-names for easy reading and added comments for all syntax in the file

--- js/flex-duck.js
+++ js/flex-duck.js
@@ -1,81 +1,119 @@
 /**
- * ===========================================
+ * =============================================
  *	FLEX DUCK
- * ===========================================
+ * =============================================
+ *	This plugin is made to make wireframing and responsive design
+ *	easy and flexible to work with, but still have strict semantic
+ *	rules that forces you to think about what elements should do.
  */
 
-function initFlexDuck(columnsAmount, mediumScreen, largeScreen) {
-	// Make sure the parameters are integers and pass the variable as null if it isn't set.
-	columnsAmount = parseInt(columnsAmount)|| null;
-	mediumScreen  = parseInt(mediumScreen) || null;
-	largeScreen   = parseInt(largeScreen)  || null;
+// Predefine variable that will help prevent unnecessary loops in: "initFlexDuck() > mediaQuery()".
+var fdChangeQuery;
 
-	// Calculates the procentages based on the "var columnsAmount"
-	function columnsPercentage(columns, name) {
-		gridArray = []; // define
+// The core for this plugin to work.
+function initFlexDuck(columnsAmount, mediumScreen, largeScreen) {
+	// Main purpose: make sure the parameters are integers, so it will strip "px" and other characters.
+	columnsAmount = parseInt(columnsAmount);
+	mediumScreen  = parseInt(mediumScreen);
+	largeScreen   = parseInt(largeScreen);
+
+	// Define class-names that will be used in the HTML (ex: small-12).
+	smallClass 	= "small";
+	mediumClass = "medium";
+	largeClass 	= "large";
+
+	// Width: calculate each column's percentage width.
+	function columnsPercentage(columns) {
+		// Declare variable as array.
+		gridArray = [];
+		// run the loop, based on the "amount of columns parameter".
 		for (i = 1; i <= columns; i++) {
-			// Calculate procentage.
+			// Calculate: basic percentage formula.
 			percentage = (100 / columns * i);
+			// Push index and percentage into array.
 			gridArray[i] = percentage;
 		}
-		// Return array
+		// Return the complete array.
 		return gridArray;
 	}
-	// Assign the returned "grid percentage array" to a variable.
+	// Assign the array from the function to a variable for later use.
 	columnWidth = columnsPercentage(columnsAmount);
 
-
+	/**
+	 * This function will add CSS values to the grid-classes (width: X %)
+	 * and hide classes with non-corresponding names.
+	 */
 	function queryStyle(className, amount, width) {
-		// Generate the classes based on the amount of
+		// Grid classes: set the width of all classes.
 		for (i = 1; i <= amount; i++) {
 			$("."+className+"-"+i).css("width", width[i]+"%");
 		};
 
-		// Remove classes
+		// Show or hide files.
 		$("[class *= '-hide']").removeClass(function(i, string) {
-			// We create an array with the classes
+			// Converts a string with all classes into an array.
 			strArray = string.split(' ');
-			// here we remove the classes we want to keep
-			return strArray.filter(function(val){
-				hiddenValidation = val.indexOf("-hidden");
-				return hiddenValidation >= 0 ? val : false;
-			}).join(' '); // and return that list as a string which indicates the classes to be removed..
+			// Will single out all the classes we want to remove and
+			// join the array-values into a string again.
+			return strArray.filter(function(singleClass){
+				// If the single class don't contain "-hidden",
+				// then the variable will be equal to "-1".
+				hiddenValidation = singleClass.indexOf("-hidden");
+				// If the variable is greater than or equal to "0", return the class-name.
+				return hiddenValidation >= 0 ? singleClass : false;
+				// .join the array into a string with spaces between the classes we want to remove.
+			}).join(' ');
 		});
-		// Add class
+
+		// Add the class "xxx-hidden" to the elements with "xxx-hide".
 		$("."+className+"-hide").addClass(className + "-hidden");
 	}
 
-	// The "media queries".
+	/**
+	 * My jQuery version of a media-query hierarchy:
+	 * This will use the browser width to check which classes
+	 * that should be instantiated.
+	 *
+	 * I defined the variable "fdChangeQuery" before the init function.
+	 * This variable will be used inside this function, so unnecessary
+	 * executions of the queryStyle() function isn't happening.
+	 * This is done to improve performance for the client.
+	 */
 	function mediaQuery() {
 		// Width of the browser window.
-		windowWidth = $(window).width();
+		browserWidth = $(window).width();
 
 		// Media query hierarchy
-		if (windowWidth >= largeScreen) {
-			/**
-			 *	Large: if the browser-window is greater than "large", then
-			 *	it will execute a function defined above with styling.
-			 */
-			queryStyle("large", columnsAmount, columnWidth);
-		} else if (windowWidth >= mediumScreen) {
-			/**
-			 *	Medium: if the browser-window is greater than "medium", then
-			 *	it will execute a function defined above with styling.
-			 */
-			queryStyle("medium", columnsAmount, columnWidth);
-		} else if (windowWidth < mediumScreen) {
-			/**
-			 *	Small: if the browser-window is less than "small", then
-			 *	it will execute a function defined above with styling.
-			 */
-			queryStyle("small", columnsAmount, columnWidth);
+		if (browserWidth >= largeScreen) { // Large
+			// Check if queryStyle() have been executed in this instance of the query.
+			if( fdChangeQuery !== 3 ) {
+				// Execute the queryStyle() function, defined earlier in the code.
+				queryStyle(largeClass, columnsAmount, columnWidth);
+				// Define value for "large query"
+				fdChangeQuery = 3;
+			}
+		} else if (browserWidth >= mediumScreen) { // Medium
+			// Check if queryStyle() have been executed in this instance of the query.
+			if( fdChangeQuery != 2 ) {
+				// Execute the queryStyle() function, defined earlier in the code.
+				queryStyle(mediumClass, columnsAmount, columnWidth);
+				// Define value for "medium query"
+				fdChangeQuery = 2;
+			}
+		} else if (browserWidth < mediumScreen) { // Small
+			// Check if queryStyle() have been executed in this instance of the query.
+			if( fdChangeQuery != 1 ) {
+				// Execute the queryStyle() function, defined earlier in the code.
+				queryStyle(smallClass, columnsAmount, columnWidth);
+				// Define value for "small query"
+				fdChangeQuery = 1;
+			}
 		}
 	}
 
-	// Init the "What to display" function on load.
+	// Init the mediaQuery on load.
 	mediaQuery();
-
-	// Init the "What to display" function on browser resize.
+	// Init the mediaQuery on browser resize.
 	$(window).resize(function() {
 		mediaQuery();
 	});
